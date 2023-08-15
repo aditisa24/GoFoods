e12f9f47bbe37fb8126dd690622832f07979c91b
Aditi Saxena <aditisa24@gmail.com>
2023-08-15 23:51:03 +0200

    removing vercel

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const connectDB = require('./db');
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://go-foods-z2vn.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     next();
@@ -17,6 +17,10 @@ app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
+
+
+//heroku
+
 
 
 app.listen(port, () => {
