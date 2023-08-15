35f1a4f29e668d8437136c1893d751e791603b0a
Aditi Saxena <aditisa24@gmail.com>
2023-08-16 00:00:54 +0200

    render

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 
 const connectDB = require('./db');
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://gofoods.onrender.com");
+    res.setHeader("Access-Control-Allow-Origin","https://gofoods.onrender.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
     next();
@@ -18,8 +18,6 @@ app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData"));
 app.use('/api', require("./Routes/OrderData"));
 
-
-//heroku
 
 
 
